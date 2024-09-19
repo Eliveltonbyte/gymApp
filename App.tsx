edeac76c88ee544379bbf5652f54d07f7647b22e
Elivelton Carneiro <eliveltonwess@gmail.com>
2024-09-18 22:15:04 -0300

    Sobe

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
-import { Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native'
 import {
   useFonts, 
   Roboto_400Regular, 
   Roboto_700Bold} 
   from "@expo-google-fonts/roboto"
+  import {GluestackUIProvider, Text} from "@gluestack-ui/themed"
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular,  Roboto_700Bold});
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      {fontsLoaded ? <Text>Home</Text> : <View />}
+    <GluestackUIProvider>
+    <View 
+    style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#202024',
+    }}
+     >
   
+    <StatusBar  
+    barStyle="light-content"
+    backgroundColor="transparent"
+    translucent
+    />
+
+    {fontsLoaded ? <Text color="white">Home</Text> : <View/>}
     </View>
+    </GluestackUIProvider>
   );
 }
 
