77b5d8356f7c04ce1af6a94188690d792abdc2ff
Elivelton Carneiro <eliveltonwess@gmail.com>
2024-09-19 23:08:44 -0300

    Criando tela Loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar, View } from 'react-native'
+import { StatusBar } from 'react-native'
 import {
   useFonts, 
   Roboto_400Regular, 
   Roboto_700Bold} 
   from "@expo-google-fonts/roboto"
   import {GluestackUIProvider, Text, Center} from "@gluestack-ui/themed"
+  
   import { config } from './config/gluestack-ui.config'
+  import { Loading } from '@components/Loading';
+  import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular,  Roboto_700Bold});
@@ -19,12 +22,7 @@ export default function App() {
     translucent
     />
 
-    {fontsLoaded ? ( 
-      <Center flex={1} bg='$gray700'>
-      <Text color="white">Home</Text>
-      </Center>  
-      ):(
-      <View/>)}
+  {fontsLoaded ? <SignIn /> : <Loading />}
    
     </GluestackUIProvider>
   );
