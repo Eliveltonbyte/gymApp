21f9e87b4f72199495bcd8be2356bc3ec4430ee9
Elivelton Carneiro <eliveltonwess@gmail.com>
2024-09-29 22:07:47 -0300

    Componenete Photo

--- src/components/HomeHearder.tsx
+++ src/components/HomeHearder.tsx
@@ -1,10 +1,15 @@
 import { Heading, HStack, VStack, Text } from "@gluestack-ui/themed";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomerHeader(){
     return (
-        <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center">
+        <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+         <UserPhoto source={{uri: "https://github.com/Eliveltonbyte.png" }}  
+         w="$16"
+         h="$16"
+         alt="Imagem do Usuário"/>
          <VStack>
-          <Text color="$gray100" fontSize="$sm">Olá</Text>
+          <Text color="$gray100" fontSize="$sm">Olá,</Text>
           <Heading color="$gray100" fontSize="$md">Elivelton Carneiro</Heading>
          </VStack>
         </HStack>
