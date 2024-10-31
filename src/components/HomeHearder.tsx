45ecc9b9e33a7a03d6c3c146c03f931c9d636493
Elivelton Carneiro <eliveltonwess@gmail.com>
2024-10-30 21:17:53 -0300

    Sobe

--- src/components/HomeHearder.tsx
+++ src/components/HomeHearder.tsx
@@ -1,5 +1,6 @@
-import { Heading, HStack, VStack, Text } from "@gluestack-ui/themed";
+import { Heading, HStack, VStack, Text, Icon } from "@gluestack-ui/themed";
 import { UserPhoto } from "./UserPhoto";
+import {LogOut} from "lucide-react-native"
 
 export function HomerHeader(){
     return (
@@ -8,10 +9,14 @@ export function HomerHeader(){
          w="$16"
          h="$16"
          alt="Imagem do Usuário"/>
-         <VStack>
+         <VStack flex={1}>
           <Text color="$gray100" fontSize="$sm">Olá,</Text>
           <Heading color="$gray100" fontSize="$md">Elivelton Carneiro</Heading>
          </VStack>
+
+       
+        <Icon as={LogOut} color="$gray200" size="xl" />
+ 
         </HStack>
     )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,14 @@
 import { VStack } from "@gluestack-ui/themed";
 
 import { HomerHeader } from "@components/HomeHearder";
+import { Group } from "@components/Group";
 
 
 export function Home(){
     return(
         <VStack flex={1}>
-             <HomerHeader />      
+             <HomerHeader />  
+             <Group name="Cpsta" isActive={false} />    
         </VStack>
     )
 }
