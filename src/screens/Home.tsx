fc481ecf844ed7ea9d227188290e4d7306addacb
Elivelton Carneiro <eliveltonwess@gmail.com>
2024-11-11 21:38:25 -0300

    Subindo mudancas

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,31 @@
-import { VStack } from "@gluestack-ui/themed";
+import { HStack, VStack } from "@gluestack-ui/themed";
 
 import { HomerHeader } from "@components/HomeHearder";
 import { Group } from "@components/Group";
+import { useState } from "react";
 
 
 export function Home(){
+    const [groupSelected, setGroupSelected] = useState("Costa")
+
     return(
         <VStack flex={1}>
              <HomerHeader />  
-             <Group name="Cpsta" isActive={false} />    
+
+             <HStack>
+             <Group name="Costa"
+              isActive={groupSelected === "costa"}
+              onPress={() => 
+              setGroupSelected("costa")}
+               />    
+
+              <Group name="Ombro"
+              isActive={groupSelected === "ombro"}
+              onPress={() => 
+              setGroupSelected("ombro")}
+               /> 
+
+             </HStack>
         </VStack>
     )
 }
